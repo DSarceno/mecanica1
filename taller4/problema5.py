b7f1231591ac9ac6fea4e9680f261f285aab5fb8
unknown <dsarceno68@gmail.com>
2020-09-08 17:04:17 -0600

    Comentarios (taller4/problema5)

--- taller4/problema5.py
+++ taller4/problema5.py
@@ -15,18 +15,28 @@ def x(t):
     return Ao*m.exp(-beta*t)*m.cos(w1*t)
 
 # SE CREAN ARRELGOS
+'''el primer arreglo define cuantos puntos de tiempo se van a valuar
+y el segundo es un arreglo conformado por ceros que luego seran sustituidos
+por el valor de la funcion para ese instante de tiempo'''
 t = np.linspace(0,50,100)
 r = np.zeros(len(t))
 
-# SE VALUA LA FUNCION
+# SE VALUA LA FUNCION (tomando cada tiempo, valuandolo y sustituyendolo en el
+# segundo arreglo)
 for i in range(len(t)):
     r[i] = x(t[i])
 
 # SE PLOTEA
+# grafica la funcion en color negro y una linea continua
 plt.plot(t,r,'-k',label='$x(t)$')
+# muestra la leyenda dada por 'label' en el comando pasado
 plt.legend()
+# nombres de cada uno de los ejes
 plt.xlabel('Tiempo')
 plt.ylabel('Posicion')
+# titulo del grafico
 plt.title('Problema 5')
+# nombre y extencion de la grafica (se guarda en la carpeta donde este el prog.)
 plt.savefig('problema5.pdf')
+# se muestra la grafica
 plt.show()
